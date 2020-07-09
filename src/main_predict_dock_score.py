dbec1b08196efe140eca0216da899b26d556f4b9
andrewdchen <ADChen@Samuels-iPad-6.dh>
2020-07-09 16:54:43 -0700

    Predict Docking score

---
+++ src/main_predict_dock_score.py
@@ -0,0 +1,30 @@
+import os
+import argparse
+
+from dataset import preprocess
+from predict_logp import predict_logp
+
+def read_args():
+    parser = argparse.ArgumentParser()
+    add_arg = parser.add_argument
+
+    add_arg('--data_path', required=True)
+    add_arg('--artifact_path', default="~/exaLearnMol/dock_score_models")
+    add_arg('--name', default="default_run")
+
+    return parser.parse_args()
+
+
+def main():
+    args = read_args()
+
+    artifact_path = os.path.join(args.artifact_path, args.name)
+    os.makedirs(artifact_path, exist_ok=True)
+
+    logp, smiles = preprocess.main(args.data_path)
+
+    predict_logp.main(artifact_path, logp, smiles, 'dock_score')
+
+
+if __name__ == "__main__":
+    main()
--- src/predict_logp/predict_logp.py
+++ src/predict_logp/predict_logp.py
@@ -13,16 +13,16 @@ from torch.utils.tensorboard import SummaryWriter
 
 import torch_geometric as pyg
 
-
 import utils.graph_utils as graph_utils
 import utils.general_utils as general_utils
 from . import model
 from .model import GNN, GNN_Dense
 
 if torch.cuda.is_available():
-    DEVICE='cuda'
+    DEVICE = 'cuda'
 else:
-    DEVICE='cpu'
+    DEVICE = 'cpu'
+
 
 #####################################################
 #                   MODEL HANDLING                  #
@@ -31,15 +31,19 @@ def load_current_model(artifact_path):
     net = torch.load(os.path.join(artifact_path, 'current_model.pth'))
     return net
 
+
 def load_best_model(artifact_path):
     net = torch.load(os.path.join(artifact_path, 'best_model.pth'))
     return net
 
+
 def save_current_model(net, artifact_path):
     torch.save(net, os.path.join(artifact_path, 'current_model.pth'))
 
+
 def save_best_model(net, artifact_path):
     torch.save(net, os.path.join(artifact_path, 'best_model.pth'))
+
 
 #############################################
 #                   DATA                    #
@@ -66,7 +70,7 @@ class MolData(Dataset):
         nb_nodes = len(g.x)
         dense_edges = get_dense_edges(len(g.x))
         g2 = pyg.data.Data(edge_index=dense_edges)
-        g2.num_nodes=nb_nodes
+        g2.num_nodes = nb_nodes
 
         return g, torch.FloatTensor([logp]), g2
 
@@ -81,8 +85,9 @@ class MolData(Dataset):
     def compute_baseline_error(self):
         logp = np.array(self.logp)
         mean = logp.mean()
-        sq_sum = np.sum(np.square(logp-mean)) / len(logp)
+        sq_sum = np.sum(np.square(logp - mean)) / len(logp)
         logging.info("{:5.3f} baseline L2 loss\n".format(sq_sum))
+
 
 def create_datasets(logp, smiles, np_seed=0):
     nb_samples = len(logp)
@@ -98,10 +103,11 @@ def create_datasets(logp, smiles, np_seed=0):
     smiles = np.asarray(smiles)[sample_order].tolist()
 
     train_data = MolData(logp[:nb_train], smiles[:nb_train])
-    valid_data = MolData(logp[nb_train:nb_train+nb_valid],
-                         smiles[nb_train:nb_train+nb_valid])
-    test_data  = MolData(logp[nb_train+nb_valid:], smiles[nb_train+nb_valid:])
+    valid_data = MolData(logp[nb_train:nb_train + nb_valid],
+                         smiles[nb_train:nb_train + nb_valid])
+    test_data = MolData(logp[nb_train + nb_valid:], smiles[nb_train + nb_valid:])
     return train_data, valid_data, test_data
+
 
 def my_collate(samples):
     g1 = [s[0] for s in samples]
@@ -113,17 +119,20 @@ def my_collate(samples):
     y = torch.cat(y, dim=0)
     return G1, y, G2
 
+
 #################################################
 #                   TRAINING                    #
 #################################################
 
 def proc_one_epoch(net,
-		   criterion,
+                   criterion,
                    batch_size,
                    loader,
                    optim=None,
                    train=False):
-    print_freq = 10 if train else 4
+    # There's only a handful of batches.
+    print_freq = 1
+    # print_freq = 10 if train else 4
     nb_batch = len(loader)
     nb_samples = nb_batch * batch_size
 
@@ -152,14 +161,14 @@ def proc_one_epoch(net,
             optim.step()
         epoch_loss += loss.item()
 
-        if ((i+1)%(nb_batch//print_freq))==0:
-            nb_proc = (i+1)*batch_size
-            logging.info("    {:8d}: {:4.2f}".format(nb_proc, epoch_loss / (i+1)))
+        if ((i + 1) % (nb_batch // print_freq)) == 0:
+            nb_proc = (i + 1) * batch_size
+            logging.info("    {:8d}: {:4.2f}".format(nb_proc, epoch_loss / (i + 1)))
         elapsed += time.time() - t1
 
     logging.info("  Model elapsed:  {:.2f}".format(elapsed))
-    logging.info("  Loader elapsed: {:.2f}".format(time.time()-t0-elapsed))
-    logging.info("  Total elapsed:  {:.2f}".format(time.time()-t0))
+    logging.info("  Loader elapsed: {:.2f}".format(time.time() - t0 - elapsed))
+    logging.info("  Total elapsed:  {:.2f}".format(time.time() - t0))
     return epoch_loss / nb_batch
 
 
@@ -172,16 +181,15 @@ def train(net,
           arg_handler,
           artifact_path,
           writer):
-
     current_lr = optim.param_groups[0]['lr']
-    lr_end = current_lr / 10**2
+    lr_end = current_lr / 10 ** 2
 
     best_loss = arg_handler('best_loss')
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optim, 'min')
     scheduler.step(best_loss)
     for i in range(arg_handler('current_epoch'), 1000):
         t0 = time.time()
-        logging.info("\n\nEpoch {}".format(i+1))
+        logging.info("\n\nEpoch {}".format(i + 1))
         logging.info("Learning rate: {0:.3g}".format(current_lr))
         logging.info("  Train:")
         train_loss = proc_one_epoch(net,
@@ -199,7 +207,7 @@ def train(net,
         logging.info("Valid MSE: {:3.2f}".format(valid_loss))
         writer.add_scalar('lr', current_lr, i)
         writer.add_scalars('loss',
-                           {'train':train_loss,'valid':valid_loss},
+                           {'train': train_loss, 'valid': valid_loss},
                            i)
         scheduler.step(valid_loss)
 
@@ -210,7 +218,7 @@ def train(net,
         logging.info("{:6.1f} seconds, this epoch".format(time.time() - t0))
 
         current_lr = optim.param_groups[0]['lr']
-        arg_handler.update_args(current_lr, i+1, best_loss)
+        arg_handler.update_args(current_lr, i + 1, best_loss)
         save_current_model(net, artifact_path)
         if current_lr < lr_end:
             break
@@ -237,14 +245,14 @@ class ArgumentHandler:
         args = {}
         args['current_epoch'] = 0
         args['current_lr'] = starting_lr
-        args['best_loss'] = 10**10
+        args['best_loss'] = 10 ** 10
         self.args = args
         self.save_args()
 
     def save_args(self):
         with open(self.arg_file, 'w') as f:
             yaml.dump(self.args, f)
-    
+
     def update_args(self, current_lr, current_epoch, best_loss):
         self.args['current_lr'] = current_lr
         self.args['current_epoch'] = current_epoch
@@ -254,6 +262,7 @@ class ArgumentHandler:
     def __call__(self, param):
         return self.args[param]
 
+
 #############################################
 #                   MAIN                    #
 #############################################
@@ -261,12 +270,13 @@ class ArgumentHandler:
 def main(artifact_path,
          logp,
          smiles,
+         task_name='predict_logp',
          batch_size=512,
          num_workers=24,
          nb_hidden=512,
          nb_layer=7,
          lr=0.001):
-    artifact_path = os.path.join(artifact_path, 'predict_logp')
+    artifact_path = os.path.join(artifact_path, task_name)
     os.makedirs(artifact_path, exist_ok=True)
     general_utils.initialize_logger(artifact_path)
 
@@ -275,6 +285,7 @@ def main(artifact_path,
     writer = SummaryWriter(log_dir=os.path.join(artifact_path, 'runs'))
 
     train_data, valid_data, test_data = create_datasets(logp, smiles)
+    # print(len(train_data), len(valid_data), len(test_data)) # Think about data augmentation.
     train_loader = DataLoader(train_data,
                               shuffle=True,
                               collate_fn=my_collate,
@@ -284,10 +295,10 @@ def main(artifact_path,
                               collate_fn=my_collate,
                               batch_size=batch_size,
                               num_workers=num_workers)
-    test_loader =  DataLoader(test_data,
-                              collate_fn=my_collate,
-                              batch_size=batch_size,
-                              num_workers=num_workers)
+    test_loader = DataLoader(test_data,
+                             collate_fn=my_collate,
+                             batch_size=batch_size,
+                             num_workers=num_workers)
 
     valid_data.compute_baseline_error()
 
@@ -295,12 +306,13 @@ def main(artifact_path,
         net = load_current_model(artifact_path)
         logging.info("Model restored")
     except Exception as e:
-        net = model.GNN_MyGAT(input_dim = train_data.get_input_dim(),
-                        nb_hidden = nb_hidden,
-                        nb_layer  = nb_layer)
+        net = model.GNN_MyGAT(input_dim=train_data.get_input_dim(),
+                              nb_hidden=nb_hidden,
+                              nb_layer=nb_layer)
         logging.info(net)
         logging.info("New model created")
     net = net.to(DEVICE)
+    print(DEVICE)
 
     optim = torch.optim.Adam(net.parameters(), lr=arg_handler('current_lr'))
     criterion = torch.nn.MSELoss()
