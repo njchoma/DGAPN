b60122deeedec9795a5f1d80a0d0a5de23136a65
andrewdchen <andrewdchen@berkeley.edu>
2020-10-06 12:51:41 -0700

    WarmStart initial commit

--- src/gcpn/PPO.py
+++ src/gcpn/PPO.py
@@ -9,6 +9,10 @@ from torch.distributions import MultivariateNormal
 from torch_geometric.data import Data, Batch
 from torch_geometric.utils import dense_to_sparse
 
+from rdkit import Chem
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
+
 from .gcpn_policy import GCPN
 
 from utils.graph_utils import state_to_pyg
@@ -347,12 +351,31 @@ def train_ppo(args, surrogate_model, env, writer=None):
             time_step +=1
             # Running policy_old:
             action = ppo.select_action(state, memory, env)
-            state, reward, done, _ = env.step(action)
+            state, reward, done, info = env.step(action)
 
             if done and (i_episode > args.surrogate_reward_timestep_delay):
                 surr_reward = get_final_reward(state, env, surrogate_model)
                 reward += surr_reward / 5
+                info['surrogate_reward'] = surr_reward
+                info['final_reward'] = reward
 
+                # From rl-baselines/baselines/ppo1/pposgd_simple_gcn.py in rl_graph_generation
+                with open('molecule_gen/'+args.name+'.csv', 'a') as f:
+                    if args.is_conditional:
+                        start_mol, end_mol = Chem.MolFromSmiles(info['start_smile']), Chem.MolFromSmiles(info['smile'])
+                        start_fingerprint, end_fingerprint = FingerprintMols.FingerprintMol(
+                            start_mol), FingerprintMols.FingerprintMol(end_mol)
+                        sim = DataStructs.TanimotoSimilarity(start_fingerprint, end_fingerprint)
+
+                        str = ''.join(['{},']*12)[:-1]+'\n'
+                        f.write(str.format(info['start_smile'], info['smile'], sim, info['reward_valid'], info['reward_qed'],\
+                                           info['reward_sa'],info['final_stat'], info['flag_steric_strain_filter'], info['flag_zinc_molecule_filter'],\
+                                           info['stop'], info['surrogate_reward'], info['final_reward']))
+                    else:
+                        str = ''.join(['{},']*10)[:-1]+'\n'
+                        f.write(str.format(info['smile'], info['reward_valid'], info['reward_qed'], info['reward_sa'],\
+                                           info['final_stat'], info['flag_steric_strain_filter'], info['flag_zinc_molecule_filter'],\
+                                           info['stop'], info['surrogate_reward'], info['final_reward']))
             
             
             # Saving reward and is_terminals:
--- src/main_gcpn.py
+++ src/main_gcpn.py
@@ -12,6 +12,7 @@ from torch.utils.tensorboard import SummaryWriter
 from utils.general_utils import maybe_download_file
 from predict_logp import model
 
+
 def molecule_arg_parser():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -29,22 +30,22 @@ def molecule_arg_parser():
     add_arg('--surrogate_reward_timestep_delay', type=int, default=0)
 
     # ENVIRONMENT PARAMETERS
-    add_arg('--dataset', type=str, default='zinc',help='caveman; grid; ba; zinc; gdb')
+    add_arg('--dataset', type=str, default='zinc', help='caveman; grid; ba; zinc; gdb')
     add_arg('--logp_ratio', type=float, default=1)
     add_arg('--qed_ratio', type=float, default=1)
     add_arg('--sa_ratio', type=float, default=1)
     add_arg('--reward_step_total', type=float, default=0.5)
     add_arg('--normalize_adj', type=int, default=0)
-    add_arg('--reward_type', type=str, default='qed',help='logppen;logp_target;qed;qedsa;qed_target;mw_target;gan')
-    add_arg('--reward_target', type=float, default=0.5,help='target reward value')
+    add_arg('--reward_type', type=str, default='qed', help='logppen;logp_target;qed;qedsa;qed_target;mw_target;gan')
+    add_arg('--reward_target', type=float, default=0.5, help='target reward value')
     add_arg('--has_feature', type=int, default=0)
-    add_arg('--is_conditional', type=int, default=0) # default 0
-    add_arg('--conditional', type=str, default='low') # default 0
-    add_arg('--max_action', type=int, default=128) # default 0
-    add_arg('--min_action', type=int, default=20) # default 0
+    add_arg('--is_conditional', type=int, default=0)  # default 0
+    add_arg('--conditional', type=str, default='')  # default 0
+    add_arg('--max_action', type=int, default=128)  # default 0
+    add_arg('--min_action', type=int, default=20)  # default 0
 
     # NETWORK PARAMETERS
-    add_arg('--emb_size', type=int, default=128) # default 64
+    add_arg('--emb_size', type=int, default=128)  # default 64
     add_arg('--layer_num_g', type=int, default=3)
     add_arg('--num_hidden_g', type=int, default=128)
     add_arg('--mlp_num_layer', type=int, default=3)
@@ -53,7 +54,6 @@ def molecule_arg_parser():
     # LOSS PARAMETERS
     add_arg('--eta', type=float, default=0.01, help='relative weight for entropy loss')
     add_arg('--upsilon', type=float, default=0.5, help='relative weight for baseline loss')
-
 
     # add_arg('--env', type=str, help='environment name: molecule; graph',
     #                     default='molecule')
@@ -97,10 +97,12 @@ def molecule_arg_parser():
 
     return parser
 
+
 def get_current_datetime():
     now = datetime.now()
     dt_string = now.strftime("%Y.%m.%d_%H:%M:%S")
     return dt_string
+
 
 def load_surrogate_model(artifact_path, surrogate_model_url, surrogate_model_path):
     if surrogate_model_url != '':
@@ -113,18 +115,24 @@ def load_surrogate_model(artifact_path, surrogate_model_url, surrogate_model_pat
     print("Surrogate model loaded")
     return surrogate_model
 
+
 def main():
     args = molecule_arg_parser().parse_args()
     print("====args====", args)
     dt = get_current_datetime()
-    writer = SummaryWriter(log_dir=os.path.join(args.artifact_path, 'runs/'+dt))
-    
+    writer = SummaryWriter(log_dir=os.path.join(args.artifact_path, 'runs/' + dt))
+
     surrogate_model = load_surrogate_model(args.artifact_path,
                                            args.surrogate_model_url,
                                            args.surrogate_model_path)
     print(surrogate_model)
 
-    train(args,surrogate_model, seed=args.seed,writer=writer)
+    # From rl-baselines/baselines/ppo1/pposgd_simple_gcn.py in rl_graph_generation
+    if not os.path.exists('molecule_gen'):
+        os.makedirs('molecule_gen')
+
+    train(args, surrogate_model, seed=args.seed, writer=writer)
+
 
 if __name__ == '__main__':
     main()
