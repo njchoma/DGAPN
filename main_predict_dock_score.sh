b474dad89ea2d47c4b3972ffe4644d06553dbed2
Nicholas Choma <nc2201@nyu.edu>
2020-12-10 09:28:33 -0800

    Keep 3d off as default

--- main_predict_dock_score.sh
+++ main_predict_dock_score.sh
@@ -7,6 +7,6 @@ PYARGS=""
 PYARGS="$PYARGS --name test_3d"
 PYARGS="$PYARGS --data_path $DATA/exaLearnMol/NSP15_6W01_A_1_F.Orderable_zinc_db_enaHLL.sorted.4col.csv"
 PYARGS="$PYARGS --artifact_path $ARTIFACTS/exaLearnMol"
-PYARGS="$PYARGS --use_3d"
+# PYARGS="$PYARGS --use_3d"
 
 python src/main_predict_dock_score.py $PYARGS
