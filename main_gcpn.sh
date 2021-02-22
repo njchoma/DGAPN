9516bdf7095be1cc242143eb9f31b6326c1fb5ae
Yulun Rayn Wu <yulun_wu@berkeley.edu>
2021-02-22 07:23:26 -0800

    setup

--- main_gcpn.sh
+++ main_gcpn.sh
@@ -6,18 +6,17 @@ conda activate my-rdkit-env
 DATA=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 PYARGS=""
-PYARGS="$PYARGS --name NSP15_6W01_A2F_crem"
+PYARGS="$PYARGS --name NSP15_6W01_A2F_crem_adversarial"
 PYARGS="$PYARGS --data_path $DATA"
 PYARGS="$PYARGS --artifact_path $DATA/gcpn"
-PYARGS="$PYARGS --use_surrogate"
+PYARGS="$PYARGS --gpu 2"
 PYARGS="$PYARGS --use_crem"
-PYARGS="$PYARGS --gpu 1"
-PYARGS="$PYARGS --surrogate_model_url https://portal.nersc.gov/project/m3623/docking_score_models/NSP15_6W01_A_2_F_neg_exp/predict_logp/best_model.pth"
 PYARGS="$PYARGS --is_conditional 1"
 PYARGS="$PYARGS --conditional /global/home/users/adchen/MD/2col/negative_only/neg_only_NSP15_6W01_A_2_F.Orderable_zinc_db_enaHLL.2col.csv"
 PYARGS="$PYARGS --use_surrogate"
 PYARGS="$PYARGS --surrogate_model_url https://portal.nersc.gov/project/m3623/docking_score_models/NSP15_6W01_A_2_F_neg_exp/predict_logp/best_model.pth"
 PYARGS="$PYARGS --surrogate_reward_episode_delay 10"
-PYARGS="$PYARGS --adversarial_reward_episode_delay 20"
+PYARGS="$PYARGS --use_adversarial"
+PYARGS="$PYARGS --adversarial_reward_episode_cutoff 1000"
 
 python src/main_gcpn.py $PYARGS
