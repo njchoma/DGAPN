79352ece0f05ab743c282a79ef6d67b6c9ec7ffd
andrewdchen <andrewdchen@berkeley.edu>
2021-02-26 14:52:25 -0800

    Update defaults for sh file

--- main_gcpn.sh
+++ main_gcpn.sh
@@ -6,15 +6,15 @@ conda activate gcpn3
 DATA=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 PYARGS=""
-PYARGS="$PYARGS --name NSP15_6W01_A1F_uncharged_kernel_dist"
+PYARGS="$PYARGS --name NSP15_6W01_A1F_crem_full_conditional"
 PYARGS="$PYARGS --data_path $DATA"
 PYARGS="$PYARGS --artifact_path $DATA/gcpn"
 PYARGS="$PYARGS --use_surrogate"
 PYARGS="$PYARGS --use_crem"
 PYARGS="$PYARGS --gpu 3"
-PYARGS="$PYARGS --surrogate_model_url "
+PYARGS="$PYARGS --surrogate_model_path /clusterfs/csdata/data/surrogate_models/NSP15_combined1_upsample/predict_logp/best_model.pth"
 PYARGS="$PYARGS --is_conditional 1"
-PYARGS="$PYARGS --conditional /global/home/users/adchen/MD/2col/uncharged_unique/uncharged_NSP15_6W01_A_1_F.Orderable_zinc_db_enaHLL.2col.csv"
+PYARGS="$PYARGS --conditional /clusterfs/csdata/data/MD/2col/uncharged_unique/NSP15_6W01_A_1_F_combined_sorted_negonly_unique1.csv"
 PYARGS="$PYARGS --surrogate_reward_timestep_delay 10"
 
 python src/main_gcpn.py $PYARGS
