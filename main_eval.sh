c8cadccb03400f6108e11b115ebb078cbc528f44
andrewdchen <andrewdchen@berkeley.edu>
2021-04-21 17:16:12 -0700

    Eval intial commit

--- main_eval.sh
+++ main_eval.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 eval "$(conda shell.bash hook)"
-conda activate my-rdkit-env_170_161
+conda activate gcpn
+
+DATA=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 PYARGS=""
-PYARGS="$PYARGS --data_path $DATA/exaLearnMol"
-# PYARGS="$PYARGS --data_path $DATA/exaLearnMol/NSP15_6W01_A_1_F.Orderable_zinc_db_enaHLL.sorted.4col.csv"
-PYARGS="$PYARGS --artifact_path $ARTIFACTS/exaLearnMol/gcpn"
-PYARGS="$PYARGS --surrogate_guide_path $ARTIFACTS/exaLearnMol/split_2/predict_logp/best_model.pth"
-PYARGS="$PYARGS --surrogate_eval_path  $ARTIFACTS/exaLearnMol/split_2/predict_logp/best_model.pth"
-PYARGS="$PYARGS --gcpn_path $ARTIFACTS/exaLearnMol/gcpn_split_1/00301_gcpn.pth"
+PYARGS="$PYARGS --warm_start_dataset_path /Users/ADChen/Downloads/MD/2col/NSP15_6W01_A_1_F_combined_sorted_negonly_unique.csv"
+PYARGS="$PYARGS --artifact_path ."
+PYARGS="$PYARGS --surrogate_guide_path artifact/surrogate_model.pth"
+PYARGS="$PYARGS --surrogate_eval_path  artifact/surrogate_model.pth"
+PYARGS="$PYARGS --gcpn_path /Users/ADChen/Downloads/saves/crem_parallel_GPU_8_2021.04.12_16:27:07/gcpn_actor_test.pth"
 
 python src/main_evaluate.py $PYARGS
