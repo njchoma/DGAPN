4a4c5fe3c19230780dd46448a5af68495a9fe531
Yulun Wu <yulun_wu@berkeley.edu>
2021-04-23 11:38:51 -0700

    merge surrogate scripts fix

--- main_gcpn.sh
+++ main_gcpn.sh
@@ -10,7 +10,7 @@ PYARGS="$PYARGS --name crem_serial"
 PYARGS="$PYARGS --gpu 1"
 PYARGS="$PYARGS --data_path $DATA/src/dataset"
 PYARGS="$PYARGS --warm_start_dataset_path $DATA/src/dataset/NSP15_6W01_A_1_F.Orderable_zinc_db_enaHLL.sorted.4col.csv"
-PYARGS="$PYARGS --artifact_path $DATA/artifact"
+PYARGS="$PYARGS --artifact_path $DATA/artifact/gcpn"
 PYARGS="$PYARGS --surrogate_model_path $DATA/artifact/best_model.pth"
 
 python src/main_gcpn.py $PYARGS
