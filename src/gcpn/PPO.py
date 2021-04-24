38056b989b1739b3a538b70b932508837c73adbf
Yulun Wu <yulun_wu@berkeley.edu>
2021-04-23 22:34:42 -0700

    merge saveing fix

--- src/gcpn/PPO.py
+++ src/gcpn/PPO.py
@@ -46,7 +46,7 @@ class Memory:
         del self.is_terminals[:]
 
 #################################################
-#                   GCPN PPO                    #
+#                    UPDATE                     #
 #################################################
 
 class DGCPN(nn.Module):
@@ -150,7 +150,7 @@ class DGCPN(nn.Module):
         else:
             return [g_emb, g_next_emb, X_states], action_logprobs, actions
 
-    def update(self, memory, save_dir, eps=1e-5):
+    def update(self, memory, eps=1e-5):
         # Monte Carlo estimate of rewards:
         rewards = []
         discounted_reward = 0
@@ -184,9 +184,6 @@ class DGCPN(nn.Module):
         rnd_loss = self.explore_critic.update(old_next_states)
         print("  RND Loss: {:7.3f}".format(rnd_loss))
 
-        # save running model
-        torch.save(self.policy.state_dict(), os.path.join(save_dir, 'running_gcpn.pth'))
-        torch.save(self.explore_critic.state_dict(), os.path.join(save_dir, 'running_rnd.pth'))
         # Copy new weights into old policy:
         self.policy_old.load_state_dict(self.policy.state_dict())
 
@@ -242,7 +239,7 @@ def train_ppo(args, surrogate_model, env):
     gamma = 0.99                # discount factor
     eta = 0.01                  # relative weight for entropy loss
 
-    lr = (5e-4, 1e-4, 2e-6)        # learning rate for actor, critic and random network
+    lr = (5e-4, 1e-4, 2e-6)     # learning rate for actor, critic and random network
     betas = (0.9, 0.999)
     eps = 0.01
 
@@ -343,12 +340,15 @@ def train_ppo(args, surrogate_model, env):
         # stop training if avg_reward > solved_reward
         if np.mean(rewbuffer_env) > solved_reward:
             print("########## Solved! ##########")
-            torch.save(ppo.policy.state_dict(), './PPO_continuous_solved_{}.pth'.format('test'))
+            torch.save(ppo.policy.actor, './PPO_continuous_solved_{}.pth'.format('test'))
             break
 
         # save every 500 episodes
         if (i_episode-1) % save_interval == 0:
-            torch.save(ppo.policy.state_dict(), os.path.join(save_dir, '{:05d}_gcpn.pth'.format(i_episode)))
+            torch.save(ppo.policy.actor, os.path.join(save_dir, '{:05d}_gcpn.pth'.format(i_episode)))
+
+        # save running model
+        torch.save(ppo.policy.actor, os.path.join(save_dir, 'running_gcpn.pth'))
 
         # logging
         if i_episode % log_interval == 0:
